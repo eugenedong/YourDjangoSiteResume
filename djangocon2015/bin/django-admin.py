3440118c54afbef276e56e9fac26dff46c12f9db
Eugene <eugene@procurify.com>
2015-09-07 09:34:26 -0500

    Virtualenv

---
+++ djangocon2015/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/eugened/Documents/GitHub/YourDjangoSiteResume/djangocon2015/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
