25e8bf56085adb3ae8a283a955e3ca8e93f106bb
Eugene <eugene@procurify.com>
2015-09-07 09:34:38 -0500

    Added Contact Info

--- app/views.py
+++ app/views.py
@@ -8,12 +8,12 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
+    'name' : 'Eugene Dong',
+    'bio' : 'Software Engineer at Procurify',
     'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
-    'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
+    'twitter_username' : 'eugened', # No @ symbol, just the handle.
+    'github_username' : "eugendong", 
+    'headshot_url' : 'https://avatars3.githubusercontent.com/u/6532589?v=3&s=460', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
 def home(request):
